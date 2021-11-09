2257928c42ebf1da6c0bb2a744c1ce11bc159124
VIJAYKALLEM <70838225+VIJAYKALLEM@users.noreply.github.com>
2021-11-08 19:02:21 -0500

    vijay_maze
    vijay_maze

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter(Collider other)
+
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Playground");
+
+        }
+        
+    }
+
+}
